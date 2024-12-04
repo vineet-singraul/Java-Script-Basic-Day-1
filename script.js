68b2936ad8dd7c74b36811876f334c204d790513
Vineet Singraul <Singraulvineet20@gmail.com>
2024-12-04 22:48:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+'use strict'
+
+// when we not define the any place vaeriable then use strict show the error and but when not writet      'use strict'  so it not show the  error
+
+
+    // int data
+    var a = 10;
+    console.log(a)
+
+    // string
+    var b = "Hello"
+    console.log(b)
+
+    // boolean
+    var c = true
+    console.log(c)
+
+    // undefined
+    var d=null;
+    console.log(d)
+    // it return the object
+
+
+    var num = 55;
+    console.log(num);
+
+
+    
+    var num2 = 55;
+    console.log(num2);
+
+
+
+    // checking for type of data type
+    console.log(typeof(a))
+    console.log(typeof(b))
+    console.log(typeof(c))
+    console.log(typeof(d))
+    console.log(typeof(num))
